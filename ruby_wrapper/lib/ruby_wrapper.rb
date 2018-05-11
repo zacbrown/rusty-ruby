989b4ad92a87745bfe25d5eaf52cef635762b18a
Zac Brown <zacbrown@users.noreply.github.com>
2018-05-10 19:38:57 -0600

    Remove extraneous file.
    Signed-off-by: Zac Brown <zacbrown@users.noreply.github.com>

--- ruby_wrapper/lib/ruby_wrapper.rb
+++ ruby_wrapper/lib/ruby_wrapper.rb
@@ -1,24 +1,6 @@
 require "ruby_wrapper/version"
 require 'ffi'
 
-module OS
-  def OS.windows?
-    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-  end
-
-  def OS.mac?
-    (/darwin/ =~ RUBY_PLATFORM) != nil
-  end
-
-  def OS.unix?
-    !OS.windows?
-  end
-
-  def OS.linux?
-    OS.unix? and not OS.mac?
-  end
-end
-
 module RubyWrapper
   extend FFI::Library
   ffi_lib 'ffi/librusty.lib'
